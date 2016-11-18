0eac555892ae9fa2d3d02f63bc2cb10662f7f2b1
TroyWhiteTW <troywhitetw@ymail.com>
2016-11-18 12:15:26 +0800

    Add Intent Page

--- app/src/main/java/com/example/troywhite/logintest/LoginActivity.java
+++ app/src/main/java/com/example/troywhite/logintest/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.troywhite.logintest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.CookieManager;
@@ -26,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 login();
+                Intent it = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(it);
             }
         });
 
@@ -35,7 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         new Thread() {
             @Override
             public void run() {
-                doLogin();
+                doLogin2();
             }
         }.start();
     }
@@ -43,7 +48,10 @@ public class LoginActivity extends AppCompatActivity {
     public void doLogin() {
         try {
             MultipartUtility_v2 mu = new MultipartUtility_v2("http://mb.sm2.xyz");
-            mu.postActPw("luby7", "aa1111");
+//            mu.postActPw("luby7", "aa1111");
+            mu.postKeyValue("username", "luby7");
+            mu.postKeyValue("password", "111111");
+            mu.postKeyValue("useragent", "mozilla/5.0 (windows nt 6.1; wow64; trident/7.0; slcc2; .net clr 2.0.50727; .net clr 3.5.30729; .net clr 3.0.30729; .net4.0c; .net4.0e; media center pc 6.0; infopath.3; rv:11.0) like gecko");
             String a = mu.getCookie();
             Log.i("troy", a);
             String[] b = a.split("; ");
@@ -57,6 +65,43 @@ public class LoginActivity extends AppCompatActivity {
                 Log.i("troy", line);
             }
         } catch (IOException e) {
+            Log.i("troy", e.toString());
+        }
+    }
+
+    public void doLogin2() {
+        try {
+            MultipartUtility_v2 mu = new MultipartUtility_v2("http://mb.sm2.xyz/ajax_login.php?action=LogApp");
+//            mu.postActPw("luby7", "111111");
+            mu.postKeyValue("username", "luby7");
+            mu.postKeyValue("password", "111111");
+            mu.postKeyValue("useragent", "mozilla/5.0 (windows nt 6.1; wow64; trident/7.0; slcc2; .net clr 2.0.50727; .net clr 3.5.30729; .net clr 3.0.30729; .net4.0c; .net4.0e; media center pc 6.0; infopath.3; rv:11.0) like gecko");
+            String a = mu.getCookie();
+            Log.i("troy", a);
+            String[] b = a.split("; ");
+            Log.i("troy", b[0]);
+            List<String> ret = mu.getHtml();
+            for (String line : ret) {
+                Log.i("troy", line);
+            }
+
+            MultipartUtility_v2 mu_2 = new MultipartUtility_v2("http://mb.sm2.xyz/mobile/wap_ajax.php?action=app_head_data");
+            mu_2.sendCookie(b[0]);
+            List<String> ret_2 = mu_2.getHtml();
+            for (String line : ret_2) {
+                Log.i("troy", line);
+            }
+            String line = ret_2.get(0);
+            JSONObject jo = new JSONObject(line);
+            String v1 = jo.getString("username");
+            Log.i("troy", v1);
+
+            MultipartUtility_v2 mu_3 = new MultipartUtility_v2("http://mb.sm2.xyz/mobile/test_wagers_fail.php?type=3");
+            mu_3.sendCookie(b[0]);
+            JSONObject aa = mu_3.getJsonData();
+            String v2 = aa.getJSONObject("list").getString("2738");
+            Log.i("troy", v2);
+        } catch (Exception e) {
             Log.i("troy", e.toString());
         }
     }
--- app/src/main/java/com/example/troywhite/logintest/MultipartUtility_v2.java
+++ app/src/main/java/com/example/troywhite/logintest/MultipartUtility_v2.java
@@ -1,5 +1,8 @@
 package com.example.troywhite.logintest;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,20 +23,28 @@ public class MultipartUtility_v2 {
     MultipartUtility_v2(String requestURL) throws IOException {
         URL url = new URL(requestURL);
         httpConn = (HttpURLConnection) url.openConnection();
-        httpConn.setReadTimeout(5000);// 设置读取超时为5秒
-        httpConn.setConnectTimeout(10000);// 设置连接网络超时为10秒
+        httpConn.setReadTimeout(5000);// 設置讀取超時為5秒
+        httpConn.setConnectTimeout(10000);// 設置連接網路超時為10秒
         httpConn.setUseCaches(false);
         httpConn.setDoOutput(true);
         httpConn.setDoInput(true);
+        httpConn.setRequestMethod("POST");// 設置請求方法為POST
     }
 
     public void postActPw(String act, String pw) throws IOException {
-        httpConn.setRequestMethod("POST");// 设置请求方法为post
+        String data = "username=" + act +
+                "&password=" + pw +
+                "&useragent=mozilla/5.0 (windows nt 6.1; wow64; trident/7.0; slcc2; .net clr 2.0.50727; .net clr 3.5.30729; .net clr 3.0.30729; .net4.0c; .net4.0e; media center pc 6.0; infopath.3; rv:11.0) like gecko";
         outputStream = httpConn.getOutputStream();
-        String data = "username=" + act + "&password=" + pw;
         outputStream.write(data.getBytes());
         outputStream.flush();
-        outputStream.close();
+    }
+
+    public void postKeyValue(String key, String value) throws IOException {
+        String data = "&" + key + "=" + value;
+        outputStream = httpConn.getOutputStream();
+        outputStream.write(data.getBytes());
+        outputStream.flush();
     }
 
     public void sendCookie(String cookie) {
@@ -55,7 +66,16 @@ public class MultipartUtility_v2 {
         return response;
     }
 
-    public void disconnect() {
+    public JSONObject getJsonData() throws IOException, JSONException {
+        String line = getHtml().get(0);
+        JSONObject jo = new JSONObject(line);
+        return jo;
+    }
+
+    public void disconnect() throws IOException {
+        outputStream.close();
         httpConn.disconnect();
     }
+
+
 }
